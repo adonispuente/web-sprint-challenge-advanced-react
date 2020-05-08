c4f947aec742754268e3d4b3d339f1ba0b496d1f
Adonis Puente <adonispuente@MacBook-Pro.attlocal.net>
2020-05-08 12:25:38 -0400

    need to do testing and questions

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -2,7 +2,29 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    };
+  }
   // add state with a property called "plants" - initialize as an empty array
+
+  componentDidMount() {
+    this.grabData();
+  }
+
+  grabData = () => {
+    axios
+      .get("http://localhost:3333/plants")
+      .then((res) => {
+        console.log(res.data.plantsData);
+        this.setState({
+          plants: res.data.plantsData,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
